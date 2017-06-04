92dfff19c1b5d5b49c7b11edb17fc3a060777f9d
Abhishek AN <basevd94@gmail.com>
2017-06-04 23:28:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ PATH=${PATH}:~/toolchains/UBERTC/aarch64-linux-android-5.3/bin
 export ARCH=arm64
 make CP8676_I02_defconfig ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-
 make -j5 ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-
-./complete.sh
+sh ./complete.sh
